bc8c6879197d3dada636b2a41c52aca52eee4574
Tabithaokoth <kobbitho@gmail.com>
2024-05-20 23:39:41 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -3,14 +3,37 @@
 def greet_programmer():
     pass
 
+def greet_programmer():
+    print("Hello, programmer!")
+    
+greet_programmer()    
+
 def greet(name):
     pass
+
+def greet(name):
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
     pass
 
+def greet_with_default(name = "programmer"):
+    print(f"Hello, {name}!")
+
+greet_with_default()    
+
 def add(num1, num2):
     pass
 
+def add (num1, num2):
+    return num1 + num2
+
+add(2, 4)
+
 def halve(number):
     pass
+def halve(number):
+    return number / 2
+
+halve(6)
